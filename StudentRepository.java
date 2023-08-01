8c4d42a7d2809fb2c0afa4fd3796111dfadab9b9
umashankar-sssit <139940766+umashankar-sssit@users.noreply.github.com>
2023-08-01 07:55:09 +0530

    Update StudentRepository.java

--- StudentRepository.java
+++ StudentRepository.java
@@ -266,6 +266,77 @@ public List<StudentPojo> RetrieveStudent() {
 		
 	}
 	
+public List<StudentPojo> RetrieveStudentById(int htno) {
+		
+		Connection con = null;
+		Statement stmt = null;	
+		ResultSet rs = null;
+		List<StudentPojo> studentList = new ArrayList<>();
+		
+		// Step 1: Loading the oracle driver into JVM		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("Driver Loaded successfully");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Failed to load driver");
+		}
+		
+		
+		try {
+			// Step 2: Establish the connection with Oracle DB
+			con = DriverManager.getConnection
+			("jdbc:oracle:thin:@localhost:1521:orcl",
+					"AdvJavaJuly23", "AdvJavaJuly23");
+			System.out.println("Connection Estd. successfully");
+			
+			// Step 3: Create a Statement
+			stmt = con.createStatement();
+			
+			// Step 4: Execute Query
+			final String SQL_QUERY = "select * from student where htno="+htno;
+			rs = stmt.executeQuery(SQL_QUERY);			
+			
+			while(rs.next()) {
+				StudentPojo stud = new StudentPojo();
+				stud.setHtno(rs.getInt("htno"));
+				stud.setsName(rs.getString("sname"));
+				stud.setM1(rs.getInt("m1"));
+				stud.setM2(rs.getInt("m2"));
+				stud.setTotal(rs.getInt("tot"));
+				stud.setAverage(rs.getDouble("average"));
+				stud.setRes(rs.getString("res"));
+				studentList.add(stud);
+			}
+			
+			
+			
+			
+		} catch (SQLException e) {
+			System.out.println("Failed to Estd. Connection");
+		}
+		
+		// Step 6: Closing all the connections in reverse order
+		finally {
+			try {
+				if(rs!=null)
+					rs.close();
+				if(stmt!=null)
+					stmt.close();
+				if(con!=null)
+					con.close();
+				
+			}
+			catch(SQLException e)
+			{
+				System.out.println(e);
+			}
+		}
+		
+		return studentList;
+		
+	}
+
+	
 	
 	private int getTotalMarks(StudentPojo stud) {
 		return stud.getM1()+stud.getM2();
